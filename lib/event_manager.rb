a0b327373079db7186aadde3864433ec9658829c
Wallebach <wallebach@proton.me>
2022-10-07 17:55:42 +0200

    Add peak hour and day of week

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -40,6 +42,34 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
+def peak_hour(dates) 
+    hours = {}
+    dates.each do | date |
+        time = Time.strptime(date, "%m/%d/%y %k:%M")
+        count = hours[time.hour]
+        if count.nil? 
+            hours[time.hour] = 1
+        else 
+            hours[time.hour] += 1
+        end
+    end
+    return hours.key(hours.values.max)
+end
+
+def peak_days(dates) 
+    weekdays = {}
+    dates.each do | date |
+        time = Time.strptime(date, "%m/%d/%y %k:%M")
+        weekday_number = weekdays[time.wday]
+        if weekday_number.nil?
+            weekdays[time.wday] = 1
+        else 
+            weekdays[time.wday] += 1
+        end
+    end
+    return weekdays.sort_by{|k,v| -v}
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -51,13 +81,16 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+dates = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   homephone = row[:homephone]
   
   homephone = clear_homephone_number(homephone)
-  # p homephone
+
+  dates << row[:regdate]
 
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
@@ -66,3 +99,8 @@ contents.each do |row|
 
   save_thank_you_letter(id,form_letter)
 end
+
+puts "Peak hour is: #{peak_hour(dates)}" 
+
+day_number = peak_days(dates)[0][0].to_i
+puts "Peak day is: #{Date::DAYNAMES[day_number]}" 
